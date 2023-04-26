b90bfa9e89b92176a64d911b5c6f25bff1bf17b6
Nicholas Smith <80678073+NixxeN001@users.noreply.github.com>
2023-04-26 11:50:46 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Code_Chanllenges
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            Console.ReadLine();
+        }
+    }
+}
